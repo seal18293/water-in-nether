f32d2a4e4df2f37c8592ab88f449c86d91be0f38
seal18293 <108873641+seal18293@users.noreply.github.com>
2025-12-07 00:10:11 +0200

    update

---
+++ water-in-nether-module-1-21-5/src/main/java/water_in_nether_module_1_21_5/mixin/IceBlockMixin.java
@@ -0,0 +1,20 @@
+package water_in_nether_module_1_21_5.mixin;
+
+import net.minecraft.block.IceBlock;
+import net.minecraft.world.dimension.DimensionType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(IceBlock.class)
+public class IceBlockMixin {
+    @Redirect(method = "afterBreak", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/dimension/DimensionType;ultrawarm()Z"))
+    private boolean func1(DimensionType instance) {
+        return false;
+    }
+
+    @Redirect(method = "melt", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/dimension/DimensionType;ultrawarm()Z"))
+    private boolean func2(DimensionType instance) {
+        return false;
+    }
+}
